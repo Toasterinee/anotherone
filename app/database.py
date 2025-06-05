c36ce2715c3479543e00d1f66ba491efae629b55
Eugene <106225930+Toasterinee@users.noreply.github.com>
2025-06-06 02:21:03 +0300

    Add files via upload

--- app/database.py
+++ app/database.py
@@ -3,9 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://fastapi_user:053089DT@localhost/myfastapidb"
+# SQLALCHEMY_DATABASE_URL = "postgresql://fastapi_user:053089DT@localhost/myfastapidb"
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://fastapi_user:tETCE5nErrF3dHgDel5NVD9jjbn5qaLA@dpg-d0qababe5dus739d7dug-a.oregon-postgres.render.com/myfastapidb_dbyp"
+SQLALCHEMY_DATABASE_URL = "postgresql://fastapi_user:tETCE5nErrF3dHgDel5NVD9jjbn5qaLA@dpg-d0qababe5dus739d7dug-a.oregon-postgres.render.com/myfastapidb_dbyp"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
